d50c71ef17f9fbbe93798c38e142f6386a93cbd5
Nagaraj Bhat <nagarajbhat12@gmail.com>
2020-07-04 01:28:19 +1000

    added gunicorn

--- app.py
+++ app.py
@@ -490,4 +490,5 @@ def prediction_update(pokemon_name,features_checklist,pokemon_type):
 
 
 if __name__ == '__main__':
-    app.run_server()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
